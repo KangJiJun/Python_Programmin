fe4379da3002966d4959e42a7e98eee0d3b31731
KangJiJun <adr.marinetpa@gmail.com>
2026-08-19 10:43:50 +0900

    Hello

---
+++ 00_io/ex1.py
@@ -0,0 +1,23 @@
+# a = input()
+# print(a)
+# print(type(a))
+
+# a=input()
+# a=int(a)
+# print(type(a))
+
+# a = int(input())
+# print(type(a))
+
+b = float(input())
+print(b, type(b))
+
+a = int(input())
+b = int(input())
+
+print(a, b)
+
+#White space sep
+lst = list(map(int,input().split()))
+for x in lst:
+    print(x,end=" ")
